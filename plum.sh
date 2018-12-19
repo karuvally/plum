3fb24e5e68413f510434bf611c819cb695a2c6bb
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2018-12-19 22:37:35 +0530

    code to check essential permissions

--- plum.sh
+++ plum.sh
@@ -1,3 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 # Plum, alpha release
 # Copyright 2018, Aswin Babu Karuvally
+
+
+# check essential stuff
+essential_stuff() {
+    # check if root
+    if [ `id -u` -ne 0 ]; then
+        echo "error: run plum as root!"
+        exit 1
+    fi
+}
+
+
+# the main function
+main() {
+    essential_stuff
+}
+
+
+# call the main function
+main
